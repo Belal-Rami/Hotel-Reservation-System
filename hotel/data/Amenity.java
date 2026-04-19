37bfd64fe21f6782e4b32df183174fbb74e14313
Shahidhamdy2006 <4e4ahomar@gmail.com>
2026-04-19 19:01:45 +0200

    Amenity,Room and Roomtype edited

--- hotel/data/Amenity.java
+++ hotel/data/Amenity.java
@@ -4,6 +4,33 @@ public class Amenity {
     private String name;
     private double price;
 public Amenity(String name){
-   this.name=name;
+    this(name, 0);
+}
+public Amenity(String name,double price){
+    setName(name);
+    setPrice(price);
+}
+public String getName(){
+    return name;
+}
+public void setName(String name){
+    if(name==null||name.isEmpty()) {
+        throw new IllegalArgumentException("Name Cannot be empty");
+    }
+    this.name=name;
+    }
+    public double getPrice(){
+    return price;
+}
+public void setPrice(double price){
+    if(price<0){
+        throw new IllegalArgumentException("Price cannot be negative");
+    }
+    this.price =price;
+}
+@Override
+public String toString() {
+    return "Amenity: " + name + " - price" + price;
 }
 }
+
--- hotel/data/RoomType.java
+++ hotel/data/RoomType.java
@@ -1,8 +1,37 @@
 package hotel.data;
 
 public class RoomType {
-    String name;
-    double price;
+    private String name;
+    private double price;
+    public RoomType(String name){
+        this(name,0);
+    }
+    public RoomType(String name,double price){
+        setName(name);
+        setPrice(price);
+    }
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        if(name==null||name.isEmpty()){
+            throw new IllegalArgumentException("Name cannot be empty");
+        }
+        this.name=name;
+    }
+    public double getPrice(){
+        return price;
+    }
+    public void setPrice(double price){
+        if(price<0){
+            throw new IllegalArgumentException("Price cannot be negative");
+        }
+        this.price=price;
+    }
+    @Override
+    public String toString() {
+        return "RoomType: " + name + " - price: " + price;
+    }
     public void singleroom(Room s){
       s.setroomtype("single");
       s.setroomprice(1000.0);
